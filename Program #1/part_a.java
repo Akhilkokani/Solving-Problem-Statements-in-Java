047db473fdbf94826057d5737183ad95f30e3169
Akhil Kokani <akhilkokani@gmail.com>
2019-01-01 19:45:16 +0530

    Documented and Segregated.

--- Program #1/part_a.java
+++ Program #1/part_a.java
@@ -14,40 +14,55 @@
 
 import java.util.Scanner;
 
-public class source {
+public class part_a {
 
   public static void main ( String[] args ) {
     
+    // Temporary variables
     String usn = "";
     String name = "";
     String branch = "";
     int phone_number = 0;
 
+    // Scanner to scan inputs from user
+    // Declaring 10 student objects
     Scanner scan = new Scanner ( System.in );
     student std[] = new student[10];
 
+    // Scanning two student details from user
     for ( int i = 0; i < 2; i++ ) {
-      
+
+      // Initialising student object
+      // Telling user which student detail he/she is entering
       std[i] = new student();
-      System.out.println ( "Enter " + i + " student data." );
-      
+      System.out.println ( "Enter " + (i + 1) + " student data." );
+
+      // USN
       System.out.print ( "Enter USN: " );
       usn = scan.nextLine();
 
+      // Name
       System.out.print ( "Enter Name: " );
       name = scan.nextLine();
 
+      // Branch
       System.out.print ( "Enter Branch: " );
       branch = scan.nextLine();
 
+      // Phone Number
       System.out.print ( "Enter Phone Number: " );
       phone_number = scan.nextInt();
 
+      // Swalloing \n character
+      // Adding student record
       scan.nextLine();
       std[i].add ( usn, name, branch, phone_number );
     }
+
+    // Closing scanner
     scan.close();
 
+    // Displaying student details
     for ( int i = 0; i < 2; i++ ) {
       System.out.println ( "Displaying " + i + " student data." );
       std[i].display();
@@ -81,7 +96,7 @@ public class student {
   }
 
   /**
-   * Used to display student details
+   * Used to display student details.
    *
    * @return void
    */
--- Program #1/part_b.java
+++ Program #1/part_b.java
@@ -6,3 +6,96 @@
  * 
  * @author Akhil Kokani
  */
+
+public class part_b {
+
+  public static void main ( String[] args ) {
+  
+    // Temporary Variables
+    int element = null;
+
+    // Scanner to scan inputs from user
+    // Stack Object
+    Scanner scan = new Scanner ( System.in );
+    Stack stack = new Stack();
+    
+    // Asking user to enter 5 elements
+    System.out.println ( "Enter 5 elements" );
+    for ( int i = 0; i < 5; i++ ) {
+
+      System.out.print ( "Enter data: " );
+
+      // Scanning and storing information into variable element
+      // Pushing element into stack
+      element = scan.nextInt();
+      stack.push ( element );
+    }
+
+    // Displaying stack contents to user
+    System.out.println ( "Displaying Stack Contents." );
+    stack.display();
+  }
+}
+
+public class Stack {
+
+  // Constant for maximum no. of elements in stack
+  // Stack Array
+  // Stack top pointer variable with -1
+  final static int max_elements_in_stack_array = 100;
+  int array[] = new int [ max_elements_in_stack_array ];
+  int top = -1;
+
+  /**
+   * Used to push (insert) element into stack.
+   *
+   * @param int element
+   * @return void
+   */
+  public void push ( int element ) {
+
+    // Stack is full
+    if ( this.top = max_elements_in_stack_array - 1 ) {
+
+      System.out.println ( "Stack Overflow." );
+      return;
+    }
+
+    this.array [ ++this.top ] = element;
+  }
+
+  /**
+   * Used to pop (remove) element from stack.
+   *
+   * @return void
+   */
+  public void pop() {
+
+    // Stack is empty
+    if ( this.top == -1 )  {
+
+      System.out.println("Stack Underflow.");
+      return;
+    }
+
+    this.top -= 1;
+  }
+
+  /**
+   * Used to display stack contents.
+   * 
+   * @return void
+   */
+  public void display() {
+
+    // Stack is empty
+    if ( this.top == -1 )
+      System.out.println("Stack Underflow.");
+
+    for ( int i = this.top; i >= 0; i-- ) {
+      System.out.format ( "%d ", this.array[i] );
+    }
+
+    System.out.print("\n");
+  }
+}
