da71b4404bec421fc4cec492c12f8f40f3b63019
Akhil Kokani <akhilkokani@gmail.com>
2019-01-13 05:22:23 +0530

    Initalised Part B

---
+++ Program #2/part_b.java
@@ -0,0 +1,10 @@
+/**
+ * PART B::
+ *  Write a Java class called Customer to store their name and date_of_birth. The date_of_birth 
+ *  format should be dd/mm/yyyy. Write methods to read customer data as <name, dd/mm/yyyy> and 
+ *  display as <name, dd, mm, yyyy> using StringTokenizer class considering the delimiter 
+ *  character as “/”.
+ *
+ * 
+ * @author Akhil Kokani
+ */
