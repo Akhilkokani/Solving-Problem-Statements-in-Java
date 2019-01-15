f54ded02c05d6eb2713ba6c329d69fa33aa932cb
Akhil Kokani <akhilkokani@gmail.com>
2019-01-15 11:38:36 +0530

    Initial Commit
    Created files and added header comments for every file.

---
+++ Program #3/part_a.java
@@ -0,0 +1,9 @@
+/**
+ * PART A::
+ *  Write a Java program to read two integers a and b. 
+ *  Compute a/b and print, when b is not zero. 
+ *  Raise an exception when b is equal to zero.
+ *
+ *
+ * @author Akhil Kokani
+ */
---
+++ Program #3/part_b.java
@@ -0,0 +1,9 @@
+/**
+ * Part B
+ *  Write a Java program that implements a multi-thread application that has three threads. 
+ *  First thread generates a random integer for every 1 second; second thread computes the 
+ *  square of the number and prints; third thread will print the value of cube of the number.
+ *
+ * 
+ * @author Akhil Kokani
+ */
