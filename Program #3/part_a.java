b416b86ac7ea8c819edc1afc10d58e34096d13ed
Akhil Kokani <akhilkokani@gmail.com>
2019-01-15 12:13:36 +0530

    Added Woking Code

--- Program #3/part_a.java
+++ Program #3/part_a.java
@@ -7,3 +7,41 @@
  *
  * @author Akhil Kokani
  */
+
+import java.util.Scanner;
+
+public class part_a {
+
+  public static void main ( String[] args ) {
+    
+    // Global Variables
+    int a, b;
+
+    // Scanner to scan inputs from user
+    Scanner scan = new Scanner ( System.in );
+
+    // Scanning a value
+    System.out.print ( "Enter a: " );
+    a = scan.nextInt();
+
+    // Scanning b value
+    System.out.print ( "Enter b: " );
+    b = scan.nextInt();
+
+    // Trying to perform operation a/b
+    // and showing result to user
+    try {
+
+      int compute = a/b;
+      System.out.println ( "a/b is: " + compute );
+    }
+    // If b is 0, catching the exception
+    // and showing to user
+    catch ( ArithmeticException Exception ) {
+      System.out.println ( "Exception Raised: " + Exception.getMessage() );
+    }
+
+    // Closing scanner
+    scan.close();
+  } 
+}
